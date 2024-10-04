a27854bf06e84151c224a8d7b9e8d2b86283320e
EricMurray-G00423903 <g00423903@atu.ie>
2024-10-04 11:02:28 +0000

    added routing and bootstrap

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,16 +1,19 @@
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-
-import Content from "./Components/content";
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './Components/NavigationBar';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Content from './Components/content';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Content />
-      <Footer />
-    </div>
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/home" element={<Content />} />
+        <Route path="/read" element={<Footer />} />
+        <Route path="/create" element={<Header />} />
+      </Routes>
+    </Router>
   );
 }
 
