559245777bb1d34b854ca4b7945334563730f8d5
EricMurray-G00423903 <g00423903@atu.ie>
2024-10-18 11:01:40 +0000

    added comments, added createjs starter functionality

--- movie-app/src/Components/Create.js
+++ movie-app/src/Components/Create.js
@@ -1,14 +1,53 @@
-// Create Component 
+// Imports
+import { useState } from "react";
 
-const Create = () => {
+// create function
+function Create() {
+    
+    //useState to handle title, year, poster || initial values set to ''
+    const [title, setTitle] = useState('');
+    const [year, setYear] = useState('');
+    const [poster, setPoster] = useState('');
 
-    // Display the create component
+    //when the submit button is clicked log the title and year and poster to the console
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(title, year, poster);
+    }
 
+
+    //on form submit go to handleSubmit() || use setTitle, year, poster funcs and values and pass event to the variables which will log to console
     return (
-        <h3>Create Component</h3>
-    )
-}
+        <div>
+        <h2>This is my Create Component.</h2>
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+            <label>Add Movie Title: </label>
+                    <input type="text"
+                    className="form-control"
+                    value={title}
+                    onChange={(e) => { setTitle(e.target.value) }}
+                />
 
+                <label>Add Movie Year: </label>
+                    <input type="text"
+                    className="form-control"
+                    value={year}
+                    onChange={(e) => { setYear(e.target.value) }}
+                />
+
+                <label>Add Movie Poster: </label>
+                    <input type="text"
+                    className="form-control"
+                    value={poster}
+                    onChange={(e) => { setPoster(e.target.value) }}
+                />
+            </div>
+            <input type="submit" value="Add Movie" />
+        </form>
+        </div>
+    );
+}
 
 // Export
 export default Create;
--- movie-app/src/Components/Read.js
+++ movie-app/src/Components/Read.js
@@ -6,23 +6,27 @@ import Movies from "./Movies";
 // Raed component - Holds JSON data about movies
 const Read = () => {
 
-      // movies
+      // movies is the variable we can use || setMovies is the function to update the variable || useState(initialised value)
       const [movies, setMovies] = useState([], );
 
+      // useEffect constantly running, page will load regardless
       useEffect(
         () => {
+          //axios get call to api page
           axios.get('https://jsonblob.com/api/jsonblob/1287718524221775872')
+          //.then to handle successful response
           .then((response) => {
-            console.log(response.data);
-            setMovies(response.data.movies);
+            console.log(response.data); // log to console
+            setMovies(response.data.movies);  // setMovies func from useState passes in the data from the response from api
           })
+          //.catch if the api call falls do some function
           .catch((error) => {
             console.log(error);
           })
         }, []
       )
 
-    // Pass data array to the Movies Component with the variable name myMovies 
+    // Pass data from the useState and api call to the movies component
     return (
       <div>
         <Movies myMovies={movies} />
