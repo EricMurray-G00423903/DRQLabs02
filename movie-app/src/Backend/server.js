df39e718efc187be18e3b75a15d5bd2dc192b67e
EricMurray-G00423903 <g00423903@atu.ie>
2024-11-16 22:13:29 +0000

    added mongoose

--- movie-app/src/Backend/server.js
+++ movie-app/src/Backend/server.js
@@ -7,6 +7,10 @@ const port = 4000;
 const cors = require('cors');
 app.use(cors());
 
+// Added mongoose
+const mongoose = require('mongoose');
+mongoose.connect('my_db_connection_string');
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
